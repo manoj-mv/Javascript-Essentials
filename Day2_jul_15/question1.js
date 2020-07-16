ebf4e42b578c9b2642d3d346e2da4dafb990c633
Manoj MV <30754843+manoj-mv@users.noreply.github.com>
2020-07-16 13:37:09 +0530

    day 2 assignment

---
+++ Day2_jul_15/question1.js
@@ -0,0 +1,5 @@
+//Write a JS code which takes input from the user and logs it in the console.?
+
+console.log('%c Question 1','color:green;font-size:20px');
+let num = prompt('Enter a number:');
+console.log(num);
---
+++ Day2_jul_15/question3.js
@@ -0,0 +1,11 @@
+/*
+Ask the user if he is 21+ and log the value 
+"Can drink", "Cannot Drink".
+*/
+console.log('%c Question 3','color:green;font-size:20px');
+let flag =confirm('Are you 21 +');
+if(flag)
+    console.log('Can drink');
+else
+    console.log('Can not drink')
+
